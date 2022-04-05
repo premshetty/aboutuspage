e3d8d82cb759efcc2f0dbf89df7d855ac84507a6
PREM KUMAR <premenaga@gmail.com>
2022-04-05 21:56:01 +0530

    Updates

---
+++ precache-manifest.a677d54248fe5bdb8ee179e8a6c174e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5b4723bb8a2f4f2330c1736fa557a9",
+    "url": "/aboutuspage/index.html"
+  },
+  {
+    "revision": "804a846780af0d7e7c74",
+    "url": "/aboutuspage/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "2585dff3afad3fc27b89",
+    "url": "/aboutuspage/static/css/main.05fec141.chunk.css"
+  },
+  {
+    "revision": "804a846780af0d7e7c74",
+    "url": "/aboutuspage/static/js/2.a1ff0a50.chunk.js"
+  },
+  {
+    "revision": "2793ce328509741e95b3",
+    "url": "/aboutuspage/static/js/3.7fa28e92.chunk.js"
+  },
+  {
+    "revision": "2585dff3afad3fc27b89",
+    "url": "/aboutuspage/static/js/main.b059de4d.chunk.js"
+  },
+  {
+    "revision": "80e124e7203ad814830f",
+    "url": "/aboutuspage/static/js/runtime~main.cf9c45f1.js"
+  }
+]);
